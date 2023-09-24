1d23814405b4db4003f0e9d159c07d995c9fb011
Fabian Bickel <fabian_bickel@outlook.at>
2023-09-24 16:37:03 +0200

    Tried fetch with JS but didn't work. Please fix

--- functions/siteRouter.js
+++ functions/siteRouter.js
@@ -58,6 +58,7 @@ siteRouter.post("/build", async (request, response) => {
 
 siteRouter.get("/sample", async (request, response) => {
   console.log(request);
+  response.status(200).json({ id: "vS18bYLIDDdmaPT7FTyj", title: "Company Landing Page"});
 });
 
 siteRouter.get("/:id", async (request, response) => {
